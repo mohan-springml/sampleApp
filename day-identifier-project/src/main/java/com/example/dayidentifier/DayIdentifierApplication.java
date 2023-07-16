f52aa00a182886ccda1db0ef98158368697277aa
Mohan <102507601+mohan-springml@users.noreply.github.com>
2023-07-16 14:28:06 +0530

    Update DayIdentifierApplication.java

--- day-identifier-project/src/main/java/com/example/dayidentifier/DayIdentifierApplication.java
+++ day-identifier-project/src/main/java/com/example/dayidentifier/DayIdentifierApplication.java
@@ -1,19 +1,20 @@
-package com.example.dayidentifier;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import java.time.DayOfWeek;
 import java.time.LocalDate;
-import java.time.format.TextStyle;
 import java.util.Locale;
 
-@SpringBootApplication
-public class DayIdentifierApplication {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-    public static void main(String[] args) {
-        SpringApplication.run(DayIdentifierApplication.class, args);
+@RestController
+@RequestMapping("/api")
+public class DayIdentifierController {
+
+    @GetMapping("/day")
+    public String getDayIdentifier() {
         LocalDate date = LocalDate.now();
-        String dayIdentifier = date.getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.getDefault());
-        System.out.println("Today is " + dayIdentifier);
+        DayOfWeek dayOfWeek = date.getDayOfWeek();
+        String dayIdentifier = dayOfWeek.getDisplayName(TextStyle.FULL, Locale.getDefault());
+        return "Today is " + dayIdentifier;
     }
 }
